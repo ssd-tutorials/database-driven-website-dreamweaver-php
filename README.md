d34f3687d90a90f6b26422cd350d69c305afcdcf
Sebastian Sulinski <Sebastian@Sebastians-Mac-Pro.local>
2015-11-19 16:59:38 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# database-driven-website-dreamweaver-php
-Exercise files for the course Database driven (dynamic) website with Dreamweaver CS3, PHP and MySQL
+# Database driven (dynamic) website with Dreamweaver CS3, PHP and MySQL
+Exercise files for the course **Database driven (dynamic) website with Dreamweaver CS3, PHP and MySQL**
---
+++ start/index.php
@@ -0,0 +1,175 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Dynamic pages using Dreamweaver CS3 by Sebastian Sulinski</title>
+<style type="text/css">
+<!--
+body {
+	font-family: Arial, Helvetica, sans-serif;
+	font-size: 10pt;
+	line-height: 1.8em;
+	margin: 0px;
+	padding-top: 20px;
+	padding-right: 0px;
+	padding-bottom: 20px;
+	padding-left: 0px;
+	text-align: center;
+	color: #666;
+}
+a:link, a:active, a:visited {
+	color: #666;
+	text-decoration: underline;
+}
+a:hover {
+	text-decoration: none;
+}
+#wrapper {
+	text-align: left;
+	padding: 0px;
+	width: 900px;
+	margin-top: 0px;
+	margin-right: auto;
+	margin-bottom: 0px;
+	margin-left: auto;
+	border: 1px solid #999;
+}
+#header {
+	margin: 0px;
+	padding: 0px;
+	height: 150px;
+	width: 100%;
+	border-bottom-width: 5px;
+	border-bottom-style: solid;
+	border-bottom-color: #999;
+	background-color: #f1f1f1;
+}
+#container {
+	margin: 0px;
+	background-attachment: scroll;
+	background-image: url(images/devider.gif);
+	background-repeat: repeat-y;
+	background-position: 200px 0px;
+	padding-top: 1px;
+	padding-right: 0px;
+	padding-bottom: 1px;
+	padding-left: 0px;
+}
+#left {
+	margin: 0px;
+	float: left;
+	width: 200px;
+	padding-top: 15px;
+	padding-right: 0px;
+	padding-bottom: 15px;
+	padding-left: 0px;
+}
+#nav {
+	margin: 0px;
+	padding: 0px;
+	list-style-type: none;
+	height: auto;
+	width: 200px;
+}
+#nav li {
+	margin: 0px;
+	padding: 0px;
+	height: 25px;
+	width: 200px;
+	background-attachment: scroll;
+	background-image: url(images/bullet.gif);
+	background-repeat: no-repeat;
+	background-position: 18px 9px;
+}
+#nav li a:link, #nav li a:visited, #nav li a:active {
+	text-decoration: none;
+	margin: 0px;
+	height: 20px;
+	width: 170px;
+	padding-top: 0px;
+	padding-right: 0px;
+	padding-bottom: 5px;
+	padding-left: 30px;
+	display: block;
+}
+#nav li a:hover {
+	color: #76A613;
+}
+#right {
+	margin: 0px;
+	float: left;
+	width: 650px;
+	padding-top: 15px;
+	padding-right: 30px;
+	padding-bottom: 0px;
+	padding-left: 20px;
+}
+#right p {
+	margin:0px;
+	padding:0px 0px 15px 0px;
+}
+#right p.title {
+	font-weight:bold;
+	color:#92B528;
+}
+.clearer {
+	margin: 0px;
+	padding: 0px;
+	clear: both;
+	height: 0px;
+	width: 100%;
+	font-size: 0px;
+	line-height: 0px;
+	display: block;
+}
+#footer {
+	margin: 0px;
+	text-align: center;
+	color: #999;
+	font-size: 12px;
+	padding-top: 15px;
+	padding-right: 0px;
+	padding-bottom: 15px;
+	padding-left: 0px;
+	border-top-width: 1px;
+	border-top-style: solid;
+	border-top-color: #999;
+}
+#footer p {
+	padding: 0px;
+	margin-top: 0px;
+	margin-right: auto;
+	margin-bottom: 0px;
+	margin-left: auto;
+}
+#footer a {
+	color: #999;
+	text-decoration: none;
+}
+-->
+</style>
+</head>
+<body>
+<div id="wrapper">
+  <div id="header"></div>
+  <div id="container">
+    <div id="left">
+      <ul id="nav">
+        <li><a href="#">link 1</a></li>
+        <li><a href="#">link 1</a></li>
+        <li><a href="#">link 1</a></li>
+        <li><a href="#">link 1</a></li>
+        <li><a href="#">link 1</a></li>
+      </ul>
+    </div>
+    <div id="right">
+      <p class="title">Title Placeholder</p>
+    </div>
+    <div class="clearer">&#160;</div>
+  </div>
+  <div id="footer">
+    <p>&#169; <a href="http://www.sebastiansulinski.co.uk/" target="_blank" title="Web Design Tutorials">Web Design Tutorials</a> by Sebastian Sulinski 
+  </div>
+</div>
+</body>
+</html>
